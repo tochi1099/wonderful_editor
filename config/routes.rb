4b1c65d5b413460a06b249390d9e2c46b7c180c3
外山大輝 <tochi1099sb@gmail.com>
2022-01-11 21:11:29 +0900

    フロントエンドの繋ぎ込み

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  namespace "api" do
-    namespace "v1" do
+  root to: "home#index"
+
+  namespace :api do
+    namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
       resources :articles
     end
