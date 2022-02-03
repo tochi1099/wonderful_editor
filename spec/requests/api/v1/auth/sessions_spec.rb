7d0d8287b27d13480cc97ed399589083277d345c
外山大輝 <tochi1099sb@gmail.com>
2022-02-04 07:43:48 +0900

    device_token_auth を使ってログアウトの実装

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -48,4 +48,30 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
       end
     end
   end
+
+  describe " DELETE api/v1/auth/sign_out " do
+    subject { delete(destroy_api_v1_user_session_path, headers: headers) }
+
+    context " ログアウトに必要な情報が送信されたとき " do
+      let(:user) { create(:user) }
+      let(:headers) { user.create_new_auth_token }
+
+      it " ログアウトできる " do
+        subject
+        expect(user.reload.tokens).to be_blank
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context " ログアウトに必要な情報が送信されなかったとき " do
+      let(:user) { create(:user) }
+      let(:headers) { { "access-token" => "nil", "client" => "nil", "uid" => "nil" } }
+
+      it " エラーする " do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["errors"]).to include("User was not found or was not logged in.")
+      end
+    end
+  end
 end
